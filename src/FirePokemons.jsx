7e5100c76d7bd3deb4c19a05d5cc7bbe91e90899
jlisita <lisita_julien@hotmail.fr>
2024-04-03 16:38:00 +0200

    add FirePokemons component

--- src/FirePokemons.jsx
+++ src/FirePokemons.jsx
@@ -72,9 +72,25 @@ const FirePokemons = () => {
           image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png",
         },
       ];
+
+      const firePokemons = pokemons.filter((element) => {
+        return (
+            element.type === "Fire"
+        );
+      });
     return (
         <section>
             <h2>Pokemons de type Feu</h2>
+            {firePokemons.map((element) => {
+                return (
+                    <>
+                        <img src={element.image} alt="image" />
+                        <h3>{element.name}</h3>
+                        <p>type: {element.type}</p>
+                        <p>date de capture: {element.capturedAt}</p>
+                    </>
+                );
+            })}
         </section>
     );
 };
