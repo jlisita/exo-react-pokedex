4a2a95e5292cc57cfef3074401ff7c4da7e88897
jlisita <lisita_julien@hotmail.fr>
2024-04-05 16:36:30 +0200

    pokedex correction

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -5,27 +5,23 @@ const Pokedex = () => {
 
     const [listPokemon, setListPokemon] = useState([]);
 
-    const newPokemon = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        let name = event.target.name.value;
-        let arr = [];
-        listPokemon.forEach((el) => { arr.push(el)})
-        arr.push(name);
-        console.log(arr)
-        setListPokemon(arr);
+        let newPokemon = event.target.name.value;
+        const newPokedex = [...listPokemon, newPokemon];
+        setListPokemon(newPokedex);
     }
-
         return (
             <section>
                 <h2>Pokédex</h2>
                 <ul>
-                    { listPokemon.map( (pokemon) => {
+                    {listPokemon.map( (pokemon) => {
                         return (
                             <li>{pokemon}</li>
                         )
                     })}
                 </ul>
-                <form onSubmit = {newPokemon}>
+                <form onSubmit = {handleSubmit}>
                     <label>Entrer le nom: <input type="text" name = "name"/></label>
                     <input type = "submit"/>
                 </form>
